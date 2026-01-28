1d49c67bb8a151221349a2b8f764d37b8c3c32d5
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2026-01-28 11:18:49 +0530

    Working with the controllers

--- HisabKitab-backend/src/controllers/auth.controller.js
+++ HisabKitab-backend/src/controllers/auth.controller.js
@@ -3,28 +3,41 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET, JWT_EXPIRES_IN } = require("../config/jwt");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+  return jwt.sign(
+
+    { id }, 
+    JWT_SECRET, 
+    { expiresIn: JWT_EXPIRES_IN }
+
+    );
 };
 
 exports.register = async (req, res) => {
   const { name, phone, password } = req.body;
 
   const exists = await User.findOne({ phone });
+
   if (exists) return res.status(400).json({ message: "User already exists" });
 
   const user = await User.create({ name, phone, password });
 
   res.status(201).json({
+
     _id: user._id,
     name: user.name,
     token: generateToken(user._id)
+    
   });
+
 };
 
 exports.login = async (req, res) => {
+  
   const { phone, password } = req.body;
 
   const user = await User.findOne({ phone });
+
   if (!user || !(await user.matchPassword(password)))
     return res.status(401).json({ message: "Invalid credentials" });
 
@@ -33,4 +46,6 @@ exports.login = async (req, res) => {
     name: user.name,
     token: generateToken(user._id)
   });
+
+
 };
