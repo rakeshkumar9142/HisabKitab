a0ff9cff217b52227c39391709a2dee65dd81fe6
Rakesh Kumar <rizul_ka_exact_github_email>
2026-03-25 16:40:26 +0530

    Added CORS and fixes

--- HisabKitab-backend/src/server.js
+++ HisabKitab-backend/src/server.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
+const cors = require("cors");
+app.use(cors({
+  origin: "*"
+}));
+
 connectDB();
 
 const PORT = process.env.PORT || 5000;
